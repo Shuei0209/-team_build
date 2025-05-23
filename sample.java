e1f80e75c328ec3a1b1fc9a5d505bcad613c999c
yagi shunsuke <yagikomeioliviashin@gmail.com>
2025-05-23 09:57:17 +0900

    java自動生成やぎ

--- sample.java
+++ sample.java
@@ -1,15 +1,35 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
+
 public class sample {
     public static void main(String[] args) {
-        String word_a = "朝から驚きの連続だ";
-        String word_i = "いつもの朝とは様子が違う";
-        String word_u = "嬉しい知らせが飛び込んできた";
-        String word_e = "駅で急に待ち合わせ。慌てて準備して";
-        String word_o = "温泉旅行に出発だ！";
+        Map<String, String[]> wordMap = new HashMap<>();
+        wordMap.put("あ", new String[]{"あなたがいるから", "あの日の約束を", "あかるい未来へ"});
+        wordMap.put("い", new String[]{"いつもそばにいて", "今、始めよう", "命を大切に"});
+        wordMap.put("う", new String[]{"運命感じて", "嬉しい気持ちを", "宇宙のように広く"});
+        wordMap.put("え", new String[]{"笑顔の花が咲く", "永遠を願って", "絵のような景色を"});
+        wordMap.put("お", new String[]{"想いを届けよう", "大きな声で", "思い出を胸に"});
 
-        System.out.println("あ：" + word_a);
-        System.out.println("い：" + word_i);
-        System.out.println("う：" + word_u);
-        System.out.println("え：" + word_e);
-        System.out.println("お：" + word_o);
+        String[] chars = {"あ", "い", "う", "え", "お"};
+        Random rand = new Random();
+
+        for (String c : chars) {
+            String[] options = wordMap.getOrDefault(c, new String[]{c + "から始まる言葉がありません"});
+            System.out.println(c + "：" + options[rand.nextInt(options.length)]);
+        }
     }
+    // public static void main(String[] args) {
+    //     String word_a = "朝から驚きの連続だ";
+    //     String word_i = "いつもの朝とは様子が違う";
+    //     String word_u = "嬉しい知らせが飛び込んできた";
+    //     String word_e = "駅で急に待ち合わせ。慌てて準備して";
+    //     String word_o = "温泉旅行に出発だ！";
+
+    //     System.out.println("あ：" + word_a);
+    //     System.out.println("い：" + word_i);
+    //     System.out.println("う：" + word_u);
+    //     System.out.println("え：" + word_e);
+    //     System.out.println("お：" + word_o);
+    // }
 }
